fec69be126e1a912a64c92c0baa6de996e964d11
Yassine Bouhaik Bouhoussaine <ybouhaik@c2r2s1.42malaga.com>
2023-07-24 16:55:05 +0200

    fill_matrix arregaldo pos_buffer y main normineado

--- bsq.c
+++ bsq.c
@@ -6,23 +6,22 @@
 /*   By: ybouhaik <ybouhaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 13:28:41 by maxgarci          #+#    #+#             */
-/*   Updated: 2023/07/24 16:00:58 by ybouhaik         ###   ########.fr       */
+/*   Updated: 2023/07/24 16:54:34 by ybouhaik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<stdlib.h>
-#include<fcntl.h>
-#include<unistd.h>
-#include<stdio.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-# define BUFFER_SIZE 2048 
+#define BUFFER_SIZE 2048
 
 int	read_file(char *file_name, int *row_count, int *column_count, char **buffer)
 {
-	int		file;
-	//char	buffer[BUFFER_SIZE];
-	int		i;
-	int		len;
+	int	file;
+	int	i;
+	int	len;
 
 	file = open(file_name, O_RDONLY);
 	if (file == -1)
@@ -46,55 +45,60 @@ int	read_file(char *file_name, int *row_count, int *column_count, char **buffer)
 	return (0);
 }
 
-char **fill_matrix(char **matrix, char *buffer)
+char	**fill_matrix(char **matrix, char *buffer)
 {
-	int row_count;
-	int column_count;
-	int pos_buffer;
-	
-	pos_buffer = 5;
-	row_count = 0;
-	column_count = 0;
+	int	row_pos;
+	int	column_pos;
+	int	pos_buffer;
+
+	pos_buffer = 0;
+	row_pos = 0;
+	column_pos = 0;
+	while (*(buffer + (pos_buffer++)) != '\n')
+		;
 	while (*(buffer + pos_buffer) != '\0')
 	{
-		if (*(buffer + pos_buffer) == 'o')
-			matrix[row_count][column_count] = 'o';
-		if (*(buffer + pos_buffer) == '.')
-			matrix[row_count][column_count] = '.';
-		if (*(buffer + pos_buffer) == '\n')
+		if ((*(buffer + pos_buffer) == 'o') || (*(buffer + pos_buffer) == '.'))
+			matrix[row_pos][column_pos] = *(buffer + pos_buffer);
+		if (*(buffer + pos_buffer++) == '\n')
 		{
-			++row_count;
-			column_count = 0;
+			++row_pos;
+			column_pos = 0;
 		}
 		else
-			++column_count;
-		pos_buffer++;
+			++column_pos;
 	}
 	return (matrix);
 }
+
+// char **put_weight_in_matrix(char **matrix, int row_count, int column_count)
+// {
+// 	int		row_pos;
+// 	int		column_pos;
+
+// 	row_count = -1;
+// 	column_count = -1;
+
+// 	while ()
+// 	return (matrix);
+// }
 
 int	main(int argc, char *argv[])
 {
 	int		row_count;
 	int		column_count;
 	char	**matrix;
-	int 	i;
+	int		i;
 	char	*buffer;
 
 	row_count = -1;
 	column_count = 0;
 	buffer = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 	i = -1;
-	if (argc < 2)
-	{
-		perror("Error en la lectura del mapa");
+	if (!argc)
 		return (1);
-	}	
 	if (read_file(argv[1], &row_count, &column_count, &buffer) == (-1))
-	{
-		perror("Fallo en la lectura del archivo: calculo filas y columnas");
 		return (1);
-	}
 	matrix = (char **)malloc(sizeof(char *) * row_count);
 	if (!matrix)
 		return (1);
@@ -104,10 +108,9 @@ int	main(int argc, char *argv[])
 		if (!(matrix[i]))
 			return (1);
 	}
-	printf("%s\n\n", buffer);
-	matrix = fill_matrix(matrix, buffer);	
-	printf ("Matriz copiada:\n");
-	i = 0;
+	matrix = fill_matrix(matrix, buffer);
+	// printf ("Matriz copiada:\n");
+	// i = 0;
 	// int j;
 	// while (i < row_count)
 	// {
@@ -120,8 +123,8 @@ int	main(int argc, char *argv[])
 	// 	printf ("\n");
 	// 	i++;
 	// }
-	while (i >= 0)
-		free(matrix[--i]);
-	free(matrix);
+	// while (i >= 0)
+	// 	free(matrix[--i]);
+	// free(matrix);
 	return (0);
 }
