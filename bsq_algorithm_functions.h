0e2e432c82e7197e3f281dd22d416f0cafe2eff4
Maximo Garcia Aroca <maxgarci@c1r6s3.42malaga.com>
2023-07-27 14:36:33 +0200

    hola

--- bsq_algorithm_functions.h
+++ bsq_algorithm_functions.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bsq3.c                                             :+:      :+:    :+:   */
+/*   bsq_algorithm_functions.h                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ybouhaik <ybouhaik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 20:31:39 by ybouhaik          #+#    #+#             */
-/*   Updated: 2023/07/25 20:42:54 by ybouhaik         ###   ########.fr       */
+/*   Updated: 2023/07/27 14:36:23 by maxgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,15 @@
 
 # include "bsq.h"
 
-struct s_param;
-struct s_pos;
+struct	s_param;
+struct	s_pos;
 
-char	**fill_matrix(char **m, char *bff, int rc, struct s_param params);
-struct s_pos	*put_obstacles(char **m, struct s_pos *obs, struct s_pos counter, struct s_param params);
-void	calc_weight(char **m, struct s_pos *obs, struct s_pos counter, struct s_pos res_p, struct s_param params);
-void	free_mat(char **matrix, struct s_pos *obs_dic, int rcnt);
+char			**fill_matrix(char **m, char *bff, int rc,
+					struct s_param params);
+struct s_pos	*put_obstacles(char **m, struct s_pos *obs,
+					struct s_pos counter, struct s_param params);
+void			calc_weight(char **m, struct s_pos *obs, struct s_pos counter,
+					struct s_pos res_p, struct s_param params);
+void			free_mat(char **matrix, struct s_pos *obs_dic, int rcnt);
 
 #endif
